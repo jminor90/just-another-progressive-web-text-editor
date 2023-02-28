d939a91de940b7e984a8eccec0c46d0b9fe86bc1
Joshua <120212106+jminor90@users.noreply.github.com>
2023-02-28 09:50:58 -0700

    Resolved install button issue

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file. **DONE**
-// TODO: Add CSS loaders and babel to webpack.
+// TODO: Add CSS loaders and babel to webpack. **Done**
 
 module.exports = () => {
   return {
